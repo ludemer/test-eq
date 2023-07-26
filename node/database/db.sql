289530167d915cfabda0d7b4bf943de9d330fc78
ludemer <55108722+ludemer@users.noreply.github.com>
2023-07-26 17:18:53 -0300

    Update db.sql

--- node/database/db.sql
+++ node/database/db.sql
@@ -1,17 +1,12 @@
-CREATE DATABASE firstapi;
-
-\l
-
-\c firstapi;
-
+--CREATE DATABASE firstapi;
+--\l
+--\c firstapi;
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     name VARCHAR(40),
     email TEXT
 );
-
 INSERT INTO users (name, email)
     VALUES ('joe', 'joe@joe.com'),
     ('luis', 'luis@luis.com');
-
 select * from users;
